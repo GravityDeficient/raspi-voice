bd37e9a0b7144189b5c33f1e483481f56b6a83c9
Shane Easton <shane.easton@gmail.com>
2025-10-28 12:41:07 -0700

    Update install_service.sh

--- scripts/install_service.sh
+++ scripts/install_service.sh
@@ -8,9 +8,6 @@ if [ ! -f "$SERVICE_FILE" ]; then
   exit 1
 fi
 
-sudo mkdir -p /home/shane/raspi-voice
-sudo cp -r "$(cd "$(dirname "$0")" && cd .. && pwd)" /home/shane/
-
 sudo cp "$SERVICE_FILE" /etc/systemd/system/raspi-voice.service
 sudo systemctl daemon-reload
 sudo systemctl enable raspi-voice.service
